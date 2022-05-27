973c27829ae495fb7fc9b4ff9066635a2984c1f4
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-27 19:59:13 +0300

    done

--- src/components/TotalAmount/TotalAmount.jsx
+++ src/components/TotalAmount/TotalAmount.jsx
@@ -1,7 +1,33 @@
 import s from './TotalAmount.module.scss';
 
-const TotalAmount = () => {
-  return <p>TotalAmount</p>;
+const TotalAmount = ({ totalCosts, totalIncomes }) => {
+  const makePrettyNumber = number => {
+    if (number === 0) {
+      return number;
+    } else if (number > 0) {
+      return `+ ${number.toLocaleString()}.00`;
+    } else if (number < 0) {
+      return `- ${Math.abs(number).toLocaleString()}.00`;
+    }
+  };
+  return (
+    <section>
+      <div className={s.wrapper}>
+        <div className={s.costsWrap}>
+          <span className={s.text}>Витрати:</span>
+          <span className={s.costsNumber}>
+            {makePrettyNumber(-totalCosts)}&nbsp;грн.
+          </span>
+        </div>
+        <div className={s.incomesWrap}>
+          <span className={s.text}>Доходи:</span>
+          <span className={s.incomesNumber}>
+            {makePrettyNumber(totalIncomes)}&nbsp;грн.
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TotalAmount;
