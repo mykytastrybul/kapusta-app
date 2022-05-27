f01bbbda7e548d50fdd6bda79fc1b22cfea6189d
s3bull <stribla@yahoo.com>
2022-05-27 12:48:38 +0300

    slices for testing

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,4 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  allUserInfo,
+  deleteTransaction,
+  expenseTransaction,
+  incomeTransaction,
+  setBalance,
+} from '../transactions/transactionsOperations';
 
 const authSlice = createSlice({
   name: 'auth',
@@ -14,7 +21,25 @@ const authSlice = createSlice({
     loading: false,
     error: null,
   },
-  reducers: {},
+  reducers: {
+    [setBalance.fulfilled](state, action) {
+      state.balance = action.payload;
+    },
+    [incomeTransaction.fulfilled](state, action) {
+      state.balance = action.payload.newBalance;
+    },
+    [expenseTransaction.fulfilled](state, action) {
+      state.balance = action.payload.newBalance;
+    },
+    [deleteTransaction.fulfilled](state, action) {
+      state.balance = action.payload.newBalance;
+    },
+    [allUserInfo.fulfilled](state, action) {
+      state.loading = false;
+      state.user.email = action.payload.email;
+      state.user.balance = action.payload.balance;
+    },
+  },
 });
 
 export default authSlice.reducer;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,16 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getExpenseCats, getIncomeCats } from './categoriesOperations';
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState: {
-    categories: {
+    data: {
       incomes: [],
       expenses: [],
     },
     loading: false,
     error: null,
   },
-  reducers: {},
+  reducers: {
+    [getIncomeCats.pending](state) {
+      state.loading = true;
+    },
+    [getIncomeCats.fulfilled](state, action) {
+      state.loading = false;
+      state.data.incomes = action.payload;
+    },
+    [getIncomeCats.rejected](state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [getExpenseCats.pending](state) {
+      state.loading = true;
+    },
+    [getExpenseCats.fulfilled](state, action) {
+      state.loading = false;
+      state.data.expenses = action.payload;
+    },
+    [getExpenseCats.rejected](state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
 export default categoriesSlice.reducer;
