8865d0501ff7d2c676d9396c88b0ecfec35ccd93
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-05-27 21:00:34 +0300

    balanceModal + balance section fix

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,8 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import s from './Balance.module.scss';
 import IconSvg from '../../assets/images/symbol-defs.svg';
+import BalanceModal from './BalanceModal';
+import { useState } from 'react';
 
 export default function Balance() {
+  const [balance, setBalance] = useState('');
+  const handleChange = e => {
+    const value = e.target.value;
+    setBalance(value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <div className={s.page}>
       <div className={s.container}>
@@ -16,9 +26,12 @@ export default function Balance() {
         </div>
         <div className={s.balance}>
           <p className={s.balance_text}>Баланс:</p>
-          <form className={s.form}>
+          <form className={s.form} onSubmit={handleSubmit}>
             <input
               className={s.input}
+              name="balance"
+              value={balance}
+              onChange={handleChange}
               type="number"
               min="0.00"
               max="10000.00"
@@ -28,6 +41,7 @@ export default function Balance() {
             <button className={s.button} type="submit">
               ПОДТВЕРДИТЬ
             </button>
+            {!balance && <BalanceModal />}
           </form>
         </div>
       </div>
