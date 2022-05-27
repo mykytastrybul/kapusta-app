8097bbb4af38f6899fbb396c630e8e84653435e0
Serhii Deineha <sergdd24041997@gmail.com>
2022-05-27 11:56:37 +0300

    added routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,44 @@
-import LoginPage from '../pages/LoginPage/LoginPage';
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import PrivateRoute from '../utils/PrivateRoute';
+import PublicRoute from '../utils/PublicRoute';
+
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+// const HomePage = lazy(() => import('../pages/CostsAndIncomesPage'));
+// const ReportPage = lazy(() => import('../pages/ReportPage'));
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute redirectTo={'/login'}>
+                {/* <HomePage /> */}
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="report"
+            element={
+              <PrivateRoute redirectTo={'/login'}>
+                {/* <ReportPage /> */}
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute restricted>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
