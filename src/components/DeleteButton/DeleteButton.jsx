70dfcf648c01c19a60745d5a5c1b6fb0b00d2011
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-30 15:07:35 +0300

    done

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,24 +1,41 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import sprite from '../../assets/images/symbol-defs.svg';
 import s from '../../components/TableIncomeOutcome/TableIncomeOutcome.module.scss';
 import { deleteTransaction } from '../../redux/transactions/transactionsOperations';
+import Modal from '../Modal/Modal';
 
 const DeleteButton = ({ id }) => {
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
-  const handleDeleteClick = e => {
+  const handleDeleteSubmit = () => {
     console.log('del transaction id', id);
     dispatch(deleteTransaction(id));
+    setShowModal(false);
   };
   return (
-    <button
-      onClick={handleDeleteClick}
-      type="button"
-      className={s['delete-btn']}
-    >
-      <svg className={s.icon} width="18" height="18">
-        <use href={`${sprite + '#icon-trashcan'}`}></use>
-      </svg>
-    </button>
+    <>
+      {showModal && (
+        <Modal
+          text="Ви впевнені?"
+          close={() => {
+            setShowModal(false);
+          }}
+          onSubmit={handleDeleteSubmit}
+        />
+      )}
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+        type="button"
+        className={s['delete-btn']}
+      >
+        <svg className={s.icon} width="18" height="18">
+          <use href={`${sprite + '#icon-trashcan'}`}></use>
+        </svg>
+      </button>
+    </>
   );
 };
 
