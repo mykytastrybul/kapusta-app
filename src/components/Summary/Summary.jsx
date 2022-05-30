78895150b3414232cee09e0f421f1ef5fe198704
OlenaPustova <i.am.elena.p@gmail.com>
2022-05-30 09:30:21 +0300

    Summary

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,38 +1,18 @@
 import s from '../../components/Summary/Summary.module.scss';
 
-export default function Summary() {
+export default function Summary(data) {
+  Object.entries(data);
+
   return (
     <div className={s.summary}>
       <h2 className={s.title}>сводка</h2>
       <ul className={s.list}>
-        <li className={s.item}>
-          <p>ноябрь</p>
-          <p>10000</p>
-        </li>
-        <li className={s.item}>
-          <p>октябрь</p>
-          <p>10000</p>
-        </li>
-        <li className={s.item}>
-          <p>сентябрь</p>
-          <p>10000</p>
-        </li>
-        <li className={s.item}>
-          <p>август</p>
-          <p>10000</p>
-        </li>
-        <li className={s.item}>
-          <p>июль</p>
-          <p>10000</p>
-        </li>
-        <li className={s.item}>
-          <p>июнь</p>
-          <p>10000</p>
-        </li>
-        <li className={s.item}>
-          <p>май</p>
-          <p>10000</p>
-        </li>
+        {Object.entries(data).map((item, idx) => (
+          <li key={idx} className={s.item}>
+            <p>{item[0]}</p>
+            <p>{item[1]}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
